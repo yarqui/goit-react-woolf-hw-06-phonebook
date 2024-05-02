23ac7ad5089d7ad56f843be73b81dbadd5107c18
yarqui <y.pelykh@gmail.com>
2024-05-01 20:24:39 -0400

    add: spinner loader

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,19 @@ import App from "./App.jsx";
 import { persistor, store } from "./redux/store.js";
 
 import "./index.css";
+import Spinner from "./components/Spinner/Spinner.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate
+        loading={
+          <div className="flex items-center justify-center">
+            <Spinner css="h-10 w-10" />
+          </div>
+        }
+        persistor={persistor}
+      >
         <App />
       </PersistGate>
     </Provider>
